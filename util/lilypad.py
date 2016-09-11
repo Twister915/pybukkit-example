e7b485b2cd811bb12fa265baebd9d2aeec524161
Joey Sacchini <joey.sacchini264@gmail.com>
2016-09-11 15:19:40 -0400

    auto commit

--- util/lilypad.py
+++ util/lilypad.py
@@ -1,6 +1,5 @@
 from lilypad.client.connect.api.request.impl import MessageRequest
 from lilypad.client.connect.api.event import MessageEvent
-from functools import wraps
 # from org.arkhamnetwork.Arkkit.patches.networkevents import NetworkEvents
 
 def get_lilypad_name():
@@ -13,7 +12,6 @@ def broadcast(channel, message):
     get_connect().request(MessageRequest([], channel, message))
 
 def listen(channel):
-    @wraps(func)
     def dec(func):
         @event_handler(MessageEvent)
         def handler(event):
