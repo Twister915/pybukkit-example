1f55e42f9ba655fab5010c0617975953969b9197
Joey Sacchini <joey.sacchini264@gmail.com>
2016-09-11 15:19:29 -0400

    auto commit

--- util/lilypad.py
+++ util/lilypad.py
@@ -1,2 +1,22 @@
+from lilypad.client.connect.api.request.impl import MessageRequest
+from lilypad.client.connect.api.event import MessageEvent
+from functools import wraps
+# from org.arkhamnetwork.Arkkit.patches.networkevents import NetworkEvents
+
 def get_lilypad_name():
-    return pyb.Bukkit.getPluginManager().getPlugin("LilyPad-Connect").getConnect().getSettings().getUsername()
+    return connect.getSettings().getUsername()
+
+def get_connect():
+    return pyb.Bukkit.getPluginManager().getPlugin("LilyPad-Connect").getConnect()
+
+def broadcast(channel, message):
+    get_connect().request(MessageRequest([], channel, message))
+
+def listen(channel):
+    @wraps(func)
+    def dec(func):
+        @event_handler(MessageEvent)
+        def handler(event):
+            if event.getChannel() == channel:
+                func(event)
+        return func
