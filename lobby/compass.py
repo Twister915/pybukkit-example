63ffa886ef6c39ee5894d1e69e7b1b6492aa2c5d
Joey Sacchini <joey.sacchini264@gmail.com>
2016-09-11 21:57:09 -0400

    auto commit

--- lobby/compass.py
+++ lobby/compass.py
@@ -5,7 +5,7 @@ from util import config
 
 @event_handler(PlayerJoinEvent)
 def on_player_join(event):
-    event.getPlayer().getInventory().setItem(0, ItemShorthand.withMaterial(Material.COMPASS).withName(color("&7&lServer Selector &7(Right Click)")).get())
+    event.getPlayer().getInventory().setItem(0, ItemShorthand.withMaterial(Material.COMPASS).withName(color("&6&lServer Selector &7(Right Click)")).get())
 
 @event_handler(PlayerInteractEvent)
 def on_interact(event):
