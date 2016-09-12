dfda736c4b1076996c385bc762dbe5cacc6573cb
Joey Sacchini <joey.sacchini264@gmail.com>
2016-09-11 21:20:10 -0400

    auto commit

--- prison/hunger.py
+++ prison/hunger.py
@@ -3,4 +3,4 @@ from org.bukkit.event.entity import FoodLevelChangeEvent
 @event_handler(FoodLevelChangeEvent)
 def on_food_change(event):
     event.getEntity().setFoodLevel(20)
-    event.setCancelled(true)
+    event.setCancelled(True)
